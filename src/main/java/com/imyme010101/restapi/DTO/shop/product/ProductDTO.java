82c4b753bd5c072d3b5ed35c87f6ba358ca781c6
imyme010101 <imyme010101@gmail.com>
2023-08-28 07:19:28 +0900

    카트 기능 부여 준비

--- src/main/java/com/imyme010101/restapi/DTO/shop/product/ProductDTO.java
+++ src/main/java/com/imyme010101/restapi/DTO/shop/product/ProductDTO.java
@@ -1,5 +1,18 @@
 package com.imyme010101.restapi.DTO.shop.product;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductDTO {
-  
+  private int idx;
+  private String title;
+  private String content;
+  private String info;
+  private Status status;
+
+  private enum Status {
+    ENABLE, DISABLE
+  }
 }
